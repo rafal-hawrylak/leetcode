ad7dc1f26d9f420f8088ba7569bbb46e621896e6
Rafal <rafal.hawrylak@gmail.com>
2023-07-19 00:31:24 +0200

    RotateArray fix

--- src/main/java/org/hawrylak/leetcode/medium/RotateArray.java
+++ src/main/java/org/hawrylak/leetcode/medium/RotateArray.java
@@ -3,35 +3,33 @@ package org.hawrylak.leetcode.medium;
 import org.hawrylak.leetcode.Solution;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class RotateArray implements Solution {
     @Override
     public Object run() {
-        int[] nums = new int[]{1,2,3,4,5,6,7};
-        rotate(nums, 3);
-        assert Arrays.equals(nums, new int[]{5, 6, 7, 1, 2, 3, 4});
+        int[] nums = new int[]{1,2,3,4,5,6};
+        rotate(nums, 4);
+        assert Arrays.equals(nums, new int[]{3,4,5,6,1,2});
         return nums;
     }
 
     private void rotate(int[] nums, int k) {
+        if (k == 0) {
+            return;
+        }
         int n = nums.length;
         int real_k = k % n;
-        boolean is_divider = n % real_k == 0;
-        int how_often_switch = n / real_k;
         if (real_k == 0) {
             return;
         }
-        int new_position = 0;
-        int old_value = nums[0];
+        int[] tmp_nums = new int[n];
         for (int i = 0; i < n; i++) {
-            if (is_divider && i > 0 && i % how_often_switch == 0) {
-                new_position++;
-                old_value = nums[new_position];
-            }
-            new_position = (new_position + real_k) % n;
-            int tmp = nums[new_position];
-            nums[new_position] = old_value;
-            old_value = tmp;
+            tmp_nums[i] = nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            int new_position = (i + real_k) % n;
+            nums[new_position] = tmp_nums[i];
         }
     }
 }
